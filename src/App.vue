c179ed96a4d16d0bd1c56db5f384478546dc285b
omer <zolcol3up@gmail.com>
2021-03-15 12:43:09 +0200

    finshed vuex todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container py-4">
+      <AddTodo />
+      <FilterTodo />
+      <todos />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import FilterTodo from './components/FilterTodo';
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+    FilterTodo,
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
